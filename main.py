6b1284026deab31ae6a473ab110c6d509fa389e9
supiccc <supiccc@gmail.com>
2018-06-11 17:30:35 +0800

    add databse

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import tornado.web
 import os
 from tornado.options import define, options
 from common.url_router import include, url_wrapper
+from models import initdb
+from sqlalchemy.orm import scoped_session, sessionmaker
+from conf.base import BaseDB, engine
 
 
 class Application(tornado.web.Application):
     def __init__(self):
+        initdb(),
         handles = url_wrapper([(r'/users/', include('views.users.users_urls'))])
         settings = dict(
             debug = True,
@@ -16,6 +20,7 @@ class Application(tornado.web.Application):
             template_path = os.path.join(os.path.dirname(__file__), "templates")
         )
         tornado.web.Application.__init__(self, handles, **settings)
+        self.db = scoped_session(sessionmaker(bind=engine,autocommit=False,autoflush=True,expire_on_commit=False))
 
 if __name__ == '__main__':
     print("Tornado server is ready for service!\r")
--- views/users/users_views.py
+++ views/users/users_views.py
@@ -5,6 +5,7 @@ import tornado.web
 from tornado.escape import json_decode
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime
 
 from common.commons import (
     http_response,
@@ -15,21 +16,31 @@ from conf.base import (
     ERROR_CODE
 )
 
+from models import (
+    Users
+)
+
 ### Configure logging ###
 logFilePath = "log/users/users.log"
 logger = logging.getLogger("Users")
 logger.setLevel(logging.DEBUG)
+# 日志整理
 handler = TimedRotatingFileHandler(logFilePath, when='D', interval=1, backupCount=30)
 formatter = logging.Formatter('%(asctime)s \
  %(filename)s [line: %(lined)d] %(levelname)s %(messages)s',)
 
-handler.suffix = "%Y%m%d" # 文件后缀名
+handler.suffix = "%Y%m%d" # 日志后缀
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
 
 class RegistHandle(tornado.web.RequestHandler):
+
+    @property
+    def db(self):
+        return self.application.db
+
     def post(self):
         try:
             args = json_decode(self.request.body)
@@ -40,5 +51,20 @@ class RegistHandle(tornado.web.RequestHandler):
             logger.info("RegistHandle: request argument incorrect")
             http_response(self, ERROR_CODE['1001'], 1001)
             return
-        logger.debug("RegistHandle: regist successfully")    
-        http_response(self, ERROR_CODE['0'], 0)
+        
+        ex_user =  self.db.query(Users).filter_by(phone = phone).first()
+        if ex_user:
+            http_response(self, ERROR_CODE['1002'], 1002)
+            self.db.close()
+            return
+        else:
+            logger.debug("RegistHandle: insert db, user: %s"%phone)
+            create_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            new_user = Users(phone, password, create_time)
+            self.db.add(new_user)
+            self.db.commit()
+            self.db.close()
+
+
+            logger.debug("RegistHandle: regist successful")
+            http_response(self, ERROR_CODE['0'], 0)
